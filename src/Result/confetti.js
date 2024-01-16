26c42c8e1a6343659eebe4386e1cf8909234827d
Nishchit-dev <nishchitpatel84@gmail.com>
2024-01-16 02:20:58 -0600

    increase number of peice in confetti

--- src/Result/confetti.js
+++ src/Result/confetti.js
@@ -5,5 +5,5 @@ export const ShowConfetti = () => {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
-  return <Confetti width={width} height={height} tweenDuration={5000} recycle={false} style={{zIndex:9999}}/>;
+  return <Confetti width={width} height={height} numberOfPieces={500} tweenDuration={5000} recycle={false} style={{zIndex:9999}}/>;
 };
