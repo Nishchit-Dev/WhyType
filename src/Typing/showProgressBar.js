48350fe0767a227e09d39beb61862de6e4fb17d4
Nishchit-dev <nishchitpatel84@gmail.com>
2024-02-23 00:30:49 -0600

    Fixed [progressBar Bug]

--- src/Typing/showProgressBar.js
+++ src/Typing/showProgressBar.js
@@ -6,19 +6,24 @@ export const ShowProgressBar = () => {
   const ProgressStats = useSelector((states) => {
     return states.TimerStatsReducer;
   });
+  const isActive = useSelector((state) => {
+    return state.typeStatsReducer.isTypingActive;
+  });
   const [width, setWidth] = useState("1%");
   useEffect(() => {
-    let TotalProgressTime = ProgressStats.ActiveFor;
-    let currentProgressTime = ProgressStats.timerCount;
-    console.log(ProgressStats.ActiveFor);
-    console.log(ProgressStats.timerCount);
+    if (!isActive) {
+      let TotalProgressTime = ProgressStats.ActiveFor;
+      let currentProgressTime = ProgressStats.timerCount;
+      console.log(ProgressStats.ActiveFor);
+      console.log(ProgressStats.timerCount);
 
-    let percentageWidth = (
-      (currentProgressTime / TotalProgressTime) *
-      100
-    ).toFixed(2);
-    setWidth((percentageWidth + "%").toString());
-  }, [ProgressStats]);
+      let percentageWidth = (
+        (currentProgressTime / TotalProgressTime) *
+        100
+      ).toFixed(2);
+      setWidth((percentageWidth + "%").toString());
+    }
+  }, [ProgressStats,isActive]);
 
   return (
     <>
